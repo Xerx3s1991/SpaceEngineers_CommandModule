5b2abaeeeaf9ad0b363ac310ca0c0ad117251e54
Xerx3s1991 <hofmann.schifflhuber.andreas@gmail.com>
2017-06-25 11:17:35 +0200

    Updated Classes

--- SpaceEngineers_Workspace/SpaceEngineers_Workspace/Source/Application/Putty.cs
+++ SpaceEngineers_Workspace/SpaceEngineers_Workspace/Source/Application/Putty.cs
@@ -16,46 +16,63 @@ using VRage.Game.ModAPI.Ingame;
 using SpaceEngineers.Game.ModAPI.Ingame;
 
 #endregion prescript
-namespace SpaceEngineers {
-    public sealed class Putty : MyGridProgram {
+namespace SpaceEngineers
+{
+    public sealed class PuttyScript : MyGridProgram
+    {
         //=======================================================================
         //////////////////////////BEGIN//////////////////////////////////////////
         //=======================================================================
-        IMyTextPanel stdin;
-        IMyTextPanel stdout;
-        /*SystemCmd[] syscmds = {
-            new SystemCmd(constructor goes here);
-        } */
-        
-        public Putty() {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script.
-            stdin = (IMyTextPanel)GridTerminalSystem.GetBlockWithName("Putty_In");
-            stdout = (IMyTextPanel)GridTerminalSystem.GetBlockWithName("Putty_Out");
-            // Check if wanted Blocks are in Grid
-            if((null == stdin) 
-                || (null == stdout))
+
+        // ----------------------------------------------------------------------------------------------- Constants
+
+        public String nameOfMasterPuttyBlock = "Insert Master Name Here";
+
+        // ----------------------------------------------------------------------------------------------- Class Defintions
+
+        // @brief:
+        // @field:
+        // @memeber:
+        public abstract class Putty : MyGridProgram
+        {
+            IMyTextPanel myPutty;
+            /*SystemCmd[] syscmds = {
+                new SystemCmd(constructor goes here);
+            } */
+
+            public Putty(String args)
             {
-                throw new ArgumentException("You must supply Putty Blocks");
+                // The constructor, called only once every session and
+                // always before any other method is called. Use it to
+                // initialize your script.
+                myPutty = (IMyTextPanel)GridTerminalSystem.GetBlockWithName(args);
+                // Check if wanted Blocks are in Grid
+                if (null == myPutty)
+                {
+                    throw new ArgumentException("You must supply Putty Block");
+                }
             }
         }
 
-        public void Main(string[] args) {
+        // @brief:
+        // @field:
+        // @memeber:
+        //public class Master(String nameOfMasterPuttyBlock) : Putty        // Why does this not work?
+        //{                                                                 // Error Messages all over the place, seems like i am to dumb to declare it correctly
+        //}
+
+
+        public void Main(string[] args)
+        {
             // The main entry point of the script, invoked every time
             // one of the programmable block's Run actions are invoked.
-            switch (stdin.GetPublicText()) {
-                case "Start Refinery":
-                    //Start the Refinery
-                    break;
-
-            }
 
             // The method itself is required, but the argument above
             // can be removed if not needed.
         }
 
-        public void Save() {
+        public void Save()
+        {
             // Called when the program needs to save its state. Use
             // this method to save your state to the Storage field
             // or some other means.
@@ -67,7 +84,7 @@ namespace SpaceEngineers {
         //=======================================================================
         //////////////////////////END////////////////////////////////////////////
         //=======================================================================
-#region postscript
-    }
-}
+        #region postscript
+    }   // Script Class
+}   // Namespace Space Engineers
 #endregion postscript
